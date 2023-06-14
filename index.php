85bbb2063781347c43604d9593d2265f2b0f5e3b
Binzen <129286646+BrunoZENARI@users.noreply.github.com>
2023-06-14 13:27:47 +0200

    fix upload_page image 1 to 5

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $result = $query->fetchAll(PDO::FETCH_ASSOC);
 				<a href="login.php" class="nav-link">Account</a>
 			</li>
 			<li class="nav-item">
-				<a href="../html/destinations.html" class="nav-link">Category</a>
+				<a href="#categ" class="nav-link">Category</a>
 			</li>
 			<li class="nav-item">
 				<a href="#section-5" class="nav-link">About</a>
@@ -28,61 +28,60 @@ $result = $query->fetchAll(PDO::FETCH_ASSOC);
 			<li class="nav-item">
 				<a href="../html/index.html#section-7" class="nav-link">Contact</a>
 			</li>
-			</ul>
-			<div class="hamburger">
-				<span class="bar"></span>
-				<span class="bar"></span>
-				<span class="bar"></span>
-			</div>
-		</nav>
-		<div class="headtitle">
-			<h1 class="title">Step into our virtual universe and embark on the ultimate gaming experience !</h1>
+		</ul>
+		<div class="hamburger">
+			<span class="bar"></span>
+			<span class="bar"></span>
+			<span class="bar"></span>
 		</div>
-		<p class="default">just a line written for you</p>
-		<div class="discover">
-			<button type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Discover</button>
-		</div>
-		<div class="heroimg">
-			<img src="./img/hero-position-img.png">
-		</div>
-	</section>
-	<section class="section-2-index">
-	 
-		<h1 class="text-center pt-10 text-5xl">Category</h1>
-		
-			
-		<div class="grid grid-flow-col pt-20 pb-20 gap-x-7 justify-center card">
-			<?php
-			//pour chaque résultat de la variable résult, on affiche le  stagiaire dans le tableau
-			foreach($result as $category){
-			?> 
+	</nav>
+	<div class="headtitle">
+		<h1 class="title">Step into our virtual universe and embark on the ultimate gaming experience !</h1>
+	</div>
+	<p class="default">just a line written for you</p>
+	<div class="discover">
+		<button type="button" class="scale-150 text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Discover</button>
+	</div>
+	<div class="heroimg">
+		<img src="./img/hero-position-img.png">
+	</div>
+</section>
+<section class="section-2-index">
+
+	<h1 class="text-center pt-10 text-5xl" id="categ">Category</h1>
+
+
+	<div class="grid grid-flow-col pt-20 pb-20 gap-x-7 justify-center card">
+		<?php
+		//pour chaque résultat de la variable résult, on affiche le  stagiaire dans le tableau
+		foreach ($result as $category) {
+		?>
+			<div>
+				<input type="hidden" name="genres" value="<?= $category['id'] ?>">
 				<div>
-					<input type="hidden" name="genres" value="<?= $category['id'] ?>">
-					<div>
-						<figure class="relative h-3/4 max-w-xl transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
-							<a href="category.php?genres=<?= $category['genres'] ?>" id="category">
-								<img class="rounded-lg" src="<?= $category['image'] ?>" alt="image description">
-							</a>
-							<figcaption class="relative px-4 text-lg text-white text-center bottom-10">
-								<p><?= $category['names'] ?></p>
-							</figcaption>
-						</figure>
-					</div>
-					
+					<figure class="relative h-3/4 max-w-xl transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+						<a href="category.php?genres=<?= $category['genres'] ?>" id="category">
+							<img class="rounded-lg" src="<?= $category['image'] ?>" alt="image description">
+						</a>
+						<figcaption class="relative px-4 text-lg text-white text-center bottom-10">
+							<p><?= $category['names'] ?></p>
+						</figcaption>
+					</figure>
 				</div>
-			<?php
-			}
-			?>	
-		</div>
-		
 
-		<button id="to-top-button" onclick="goToTop()" title="Go To Top"
-		class="hidden fixed z-90 bottom-8 right-8 border-0 w-16 h-16 rounded-full drop-shadow-md bg-indigo-500 text-white text-3xl font-bold">&uarr;</button>
+			</div>
+		<?php
+		}
+		?>
+	</div>
 
-	
 
-	</section>
+	<button id="to-top-button" onclick="goToTop()" title="Go To Top" class="hidden fixed z-90 bottom-8 right-8 border-0 w-16 h-16 rounded-full drop-shadow-md bg-indigo-500 text-white text-3xl font-bold">&uarr;</button>
 
-	<?php 
-	include "includes/footer.php";
+
+
+</section>
+
+<?php
+include "includes/footer.php";
 ?>
--- upload.php
+++ upload.php
@@ -3,6 +3,12 @@ require_once('connect.php');
 session_start();
 include "./includes/header.php";
 
+    $sql = "SELECT names FROM products";
+	$query = $db->prepare($sql);
+	$query->execute();
+	$result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+
 // Vérification si un fichier a été envoyé
 if (isset($_FILES["image"]) && $_FILES["image"]["error"] === 0) {
     // on a reçu l'image
@@ -45,6 +51,7 @@ if (isset($_FILES["image"]) && $_FILES["image"]["error"] === 0) {
 
     if (isset($_POST['image'])){
         $image = $_POST['image'];
+        
 
         $imageToUpdate = '';
         switch ($image) {
@@ -67,11 +74,12 @@ if (isset($_FILES["image"]) && $_FILES["image"]["error"] === 0) {
     
         if (!empty($imageToUpdate)){
             //upload image to db
-        
-            $sql = ("UPDATE products SET $imageToUpdate = :image WHERE names = :filename");
+            $nameGame = $_POST['names'];
+
+            $sql = ("UPDATE products SET $imageToUpdate = :newfilename WHERE names = :nameGame");
             $stmt = $db->prepare($sql);
             $stmt->bindParam(':newfilename', $newfilename);
-            $stmt->bindParam(':filename', $filename);
+            $stmt->bindParam(':nameGame', $nameGame);
             if ($stmt->execute()) {
                 header('location: users.php');
                 echo 'fichier télécharger';
@@ -118,46 +126,63 @@ if (isset($_FILES["image"]) && $_FILES["image"]["error"] === 0) {
             </div>
         </nav>
         <div class="flex flex-col items-center justify-center px-2 py-4 mx-auto  lg:py-0">
-            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-xl xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         Add Image
                     </h1>
-                    <form action="" method="post" enctype="multipart/form-data" class="space-y-4 md:space-y-6">
-                        <div>
-                            <label for="image" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image Number choice</label>
-                            <ul class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-                                <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                                    <div class="flex items-center pl-3">
-                                        <input id="image1" type="radio" value="image1" name="image" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                                        <label for="image1" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Image n°1</label>
-                                    </div>
-                                </li>
-                                <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                                    <div class="flex items-center pl-3">
-                                        <input id="image2" type="radio" value="image2" name="image" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                                        <label for="image2" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Image n°2</label>
-                                    </div>
-                                </li>
-                                <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                                    <div class="flex items-center pl-3">
-                                        <input id="image3" type="radio" value="image3" name="image" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                                        <label for="image3" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Image n°3</label>
-                                    </div>
-                                </li>
-                                <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                                    <div class="flex items-center pl-3">
-                                        <input id="image4" type="radio" value="image4" name="image" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                                        <label for="image4" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Image n°4</label>
-                                    </div>
-                                </li>
-                                <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                                    <div class="flex items-center pl-3">
-                                        <input id="image5" type="radio" value="image5" name="image" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                                        <label for="image5" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Image n°5</label>
-                                    </div>
-                                </li>
-                            </ul>
+                    <form action="" method="post" enctype="multipart/form-data" class="space-y-4 md:space-y-6" id="uploadImage">
+                        <div class="flex flex-row">
+                            <div>
+                                <label for="names">Choose a game :</label>
+                                
+                                <select name="names" id="names" required form="uploadImage">
+                                    <option value="">--- Choose a game ---</option>
+                                    <?php
+                                        foreach ($result as $game) {
+                                    ?>
+                                    <option value="<?= $game['names'] ?>"><?= $game['names'] ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                                
+                            </div>
+                            <div>
+                                <label for="image" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image Number choice</label>
+                                <ul class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                                    <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                                        <div class="flex items-center pl-3">
+                                            <input id="image1" type="radio" value="image1" name="image" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                            <label for="image1" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Image n°1</label>
+                                        </div>
+                                    </li>
+                                    <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                                        <div class="flex items-center pl-3">
+                                            <input id="image2" type="radio" value="image2" name="image" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                            <label for="image2" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Image n°2</label>
+                                        </div>
+                                    </li>
+                                    <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                                        <div class="flex items-center pl-3">
+                                            <input id="image3" type="radio" value="image3" name="image" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                            <label for="image3" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Image n°3</label>
+                                        </div>
+                                    </li>
+                                    <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                                        <div class="flex items-center pl-3">
+                                            <input id="image4" type="radio" value="image4" name="image" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                            <label for="image4" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Image n°4</label>
+                                        </div>
+                                    </li>
+                                    <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                                        <div class="flex items-center pl-3">
+                                            <input id="image5" type="radio" value="image5" name="image" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                            <label for="image5" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Image n°5</label>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                         <div>
                             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="fichier">Image</label>
